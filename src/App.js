ce3f5fac884416f941ea056546f3871764342750
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-04-13 13:00:58 -0300

    Github OAuth App

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
-
+import Users from './components/users/Users';
+import axios from 'axios';
 
 export class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
-      <React.Fragment>
-      <Navbar />
-      <UserItem />
-      </React.Fragment>
-      );
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
   }
 }
 
